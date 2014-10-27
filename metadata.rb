1fb805e13a2587fa62d781cad4550d45fe364abd
Jamie <jamie@stoic.net>
2014-10-27 11:41:51 -0700

    Berkshelf / Ridley requires the "name" attribute.
    Ridley::Errors::MissingNameAttribute The metadata at '/tmp/d20141027-2933-5qrvwr' does not contain a 'name' attribute. While Chef does not strictly enforce this requirement, Ridley cannot continue without a valid metadata 'name' entry.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "Mark Roddy"
 maintainer_email "^^ at gmail"
 license          "apache"
+name             "gsettings"
 description      "Configure Gnome via gsettings"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
